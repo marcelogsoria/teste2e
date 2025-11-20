9ee9debb98f96d62b57aae3c52baf69c89faf18a
Marcelo Soria <msoria@appsolut.us>
2025-11-20 12:31:30 -0300

    test

---
+++ cypress/support/e2e.js
@@ -0,0 +1,21 @@
+// ***********************************************************
+// Este archivo es procesado y cargado automáticamente antes
+// de ejecutar los tests. Aquí puedes poner comandos globales
+// y configuración que quieras que esté disponible para todos
+// tus tests.
+//
+// Puedes cambiar la ubicación de este archivo o desactivarlo
+// usando la opción 'supportFile' en la configuración de Cypress.
+//
+// Más información: https://on.cypress.io/configuration
+// ***********************************************************
+
+// Importar comandos personalizados si los tienes
+// import './commands'
+
+// Opcionalmente puedes desactivar el manejo de excepciones no capturadas
+// para que Cypress las capture automáticamente
+// Cypress.on('uncaught:exception', (err, runnable) => {
+//   // retornar false aquí previene que Cypress falle el test
+//   return false
+// })
